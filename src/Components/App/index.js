55db38fc10e460cb32242e3a66feb3124e933d21
hung <dshung1997@gmail.com>
2019-04-01 05:28:07 +0200

    Add a SearchBox dummy

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ListChampions from '../ListChampions';
+import SearchBox from '../SearchBox';
 
 class App extends Component {
   render() {
     return (
         <div className='bg-dark'>
-            {/* <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header> */}
-
+            <SearchBox />
             <ListChampions/>
         </div>
     );
---
+++ src/Components/SearchBox/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SearchBox.css';
+
+class SearchBox extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" name="name" />
+            </div>
+        );
+    }
+}
+
+export default SearchBox;

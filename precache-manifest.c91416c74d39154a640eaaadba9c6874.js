7e9c18afbaabaaa08da04ec8e56c9f6d855071e4
hung <dshung1997@gmail.com>
2019-04-06 22:09:14 +0200

    Updates

---
+++ precache-manifest.c91416c74d39154a640eaaadba9c6874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "401817ac1b7625cc8c3c",
+    "url": "/LOLChamps/static/js/runtime~main.4f3e53af.js"
+  },
+  {
+    "revision": "a5ee2d3f871e137a13e3",
+    "url": "/LOLChamps/static/js/main.87f0bf1a.chunk.js"
+  },
+  {
+    "revision": "64c2804dec6a9b423657",
+    "url": "/LOLChamps/static/js/2.8d58a39e.chunk.js"
+  },
+  {
+    "revision": "a5ee2d3f871e137a13e3",
+    "url": "/LOLChamps/static/css/main.187b30c4.chunk.css"
+  },
+  {
+    "revision": "3ed43cb5db7ef9320a85bb2c222209cf",
+    "url": "/LOLChamps/index.html"
+  }
+];
